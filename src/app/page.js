e4786071470c2b7a9cabed870e09d36c9951e5c1
cqlyj <luoyingjie0721@gmail.com>
2024-03-20 03:44:23 +0800

    Update layout and add LotteryEntrance component

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,15 @@
 import Image from "next/image";
 import Header from "./components/Header";
 import { MoralisProvider } from "react-moralis";
+import LotteryEntrance from "./components/LotteryEntrance";
 
 export default function Home() {
   return (
     <>
       <MoralisProvider initializeOnMount={false}>
         <Header />
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          Hello, world!
-        </main>
+        <LotteryEntrance />
+        <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
       </MoralisProvider>
     </>
   );
